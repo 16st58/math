a6303372cae6a7b77f1aafe09208c6477d700531
16st58 <105636718+16st58@users.noreply.github.com>
2022-12-27 11:41:12 +0900

    커밋메세지

---
+++ 111/math/Assets/Script/Enemy.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public ParticleSystem explosionParticle;
+    public float moveSpeed = 0.01f;
+    public float health = 10.0f;
+    public float bulletDamage = 1.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //적을 플레이어 위치로 이동
+        transform.position = Vector3.MoveTowards(gameObject.transform.position, GameObject.Find("Player").transform.position, moveSpeed);  
+    }
+ 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //총알에 닿았을때
+        if (other.tag == "Bullet")
+        {
+            //Play Particle
+            //ParticleSystem instance = Instantiate(explosionParticle, transform.position, Quaternion.identity);
+            //instance.Play();
+            //Destroy(instance.gameObject, instance.main.duration);
+ 
+            //Take Damage
+            health -= bulletDamage;
+            if (health <= 0) {
+                //Set Active off
+                Match.enemyNum -= 1;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ 111/math/Assets/Script/Match.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class Match : MonoBehaviour
+{
+    public static int enemyNum = 0;
+    public GameObject enemyObject;//적 오브젝트
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemyNum <= 0) {//TODO
+            stage(10);
+        }
+    }
+    /*
+    스테이지 시작할떄 적을 생성하는 함수
+    */
+    void stage(int num)
+    {
+        enemyNum = num;
+        int random = 0;
+        Vector3 position;
+
+        //지정한 수만큼 적 복제
+        for(int i = 0; i < num; i++){
+
+            //랜덤으로 적의 위치 지정
+            random = Random.Range(0, 4);
+            if (random == 0){
+                position = new Vector3(Random.Range(-40.0f, 40.0f), 20, 0);
+            } else if (random == 1) {
+                position = new Vector3(Random.Range(-40.0f, 40.0f), -20, 0);
+            } else if (random == 2) {
+                position = new Vector3(40, Random.Range(-20.0f, 20.0f), 0);
+            } else {
+                position = new Vector3(-40, Random.Range(-20.0f, 20.0f), 0);
+            }
+
+            //적 복제
+            GameObject enemy = Instantiate(enemyObject, position, transform.rotation);
+            Rigidbody2D rigid_enemy = enemy.GetComponent<Rigidbody2D>();
+            Enemy enemy1 = enemy.GetComponent<Enemy>();
+            enemy1.moveSpeed = Random.Range(0.01f, 0.03f);
+            enemy1.health = Random.Range(5.0f, 15.0f);
+        }
+    }
+}
